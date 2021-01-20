42f11b78840148d653182005384ff06cdeee6c6d
James Robinson <james.em.robinson@gmail.com>
2021-01-20 15:20:15 +0000

    Include all ch04 artifacts except eggs and doctrees

---
+++ ch04packaging/greetings/build/lib/greetings/greeter.py
@@ -0,0 +1,26 @@
+def greet(personal, family, title="", polite=False):
+    """Generate a greeting string for a person.
+
+    Parameters
+    ----------
+    personal: str
+        A given name, such as Will or Jean-Luc
+    family: str
+        A family name, such as Riker or Picard
+    title: str
+        An optional title, such as Captain or Reverend
+    polite: bool
+        True for a formal greeting, False for informal.
+
+    Returns
+    -------
+    string
+        An appropriate greeting
+    """
+
+    greeting = "How do you do, " if polite else "Hey, "
+    if title:
+        greeting += title + " "
+
+    greeting += personal + " " + family + "."
+    return greeting
