2323d596232da5a15e52bb41112ff5ccf97d4213
James Robinson <james.em.robinson@gmail.com>
2021-08-11 17:55:15 +0100

    :bug: Fix the GitHub Pages Jekyll build

--- build_docs.sh
+++ build_docs.sh
@@ -8,3 +8,7 @@ jupyter-book clean --html .
 # Build the HTML output and link the redirect page
 make html
 cp docs/index.html _build
+
+# Add the .nojekyll directive to stop GitHub Pages excluding directories with underscores
+# See https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#static-site-generators
+touch _build/.nojekyll
