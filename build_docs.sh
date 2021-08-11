39ff4f3a9174b99306b72f07ed54ec6380c06f37
James Robinson <james.em.robinson@gmail.com>
2021-08-11 18:03:51 +0100

    :bug: Add a second .nojekyll directive which seems to be required

--- build_docs.sh
+++ build_docs.sh
@@ -12,3 +12,4 @@ cp docs/index.html _build
 # Add the .nojekyll directive to stop GitHub Pages excluding directories with underscores
 # See https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#static-site-generators
 touch _build/.nojekyll
+touch _build/html/.nojekyll
