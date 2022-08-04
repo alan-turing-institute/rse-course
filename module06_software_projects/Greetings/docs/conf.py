663bfe62d3570067a3c0512bef8a1a5466d71db9
jack89roberts <jroberts@turing.ac.uk>
2022-08-04 14:05:31 +0100

    :truck: doc -> docs

--- module06_software_projects/Greetings/docs/conf.py
+++ module06_software_projects/Greetings/docs/conf.py
@@ -1,11 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
 # -- Project information -----------------------------------------------------
 
-project = u"Greetings"
-copyright = u"2021, James Hetherington"
-author = "James Hetherington"
+project = "greetings"
+copyright = "2022, The Alan Turing Institute"
+author = "The Alan Turing Institute"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,23 +44,14 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["Thumbs.db", ".DS_Store"]
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-source_suffix = ".rst"
-
-# The master toctree document.
-master_doc = "index"
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+#
 html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -63,8 +71,8 @@ latex_documents = [
     (
         "index",
         "Greetings.tex",
-        u"Greetings Documentation",
-        u"James Hetherington",
+        "Greetings Documentation",
+        "James Hetherington",
         "manual",
     ),
 ]
@@ -75,7 +83,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ("index", "greetings", u"Greetings Documentation", [u"James Hetherington"], 1)
+    ("index", "greetings", "Greetings Documentation", ["James Hetherington"], 1)
 ]
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -87,8 +95,8 @@ texinfo_documents = [
     (
         "index",
         "Greetings",
-        u"Greetings Documentation",
-        u"James Hetherington",
+        "Greetings Documentation",
+        "James Hetherington",
         "Greetings",
         "One line description of project.",
         "Miscellaneous",
